fe4e3dbad13f8dae2171869a087eae820a4eca51
@nadezhda.fedotova <ub-326@ub-326s-MacBook-Pro.local>
2023-03-12 20:01:35 +0200

    ДЗ 29. Розділення логіки

---
+++ js/constans.js
@@ -0,0 +1,68 @@
+export const countOffers = 10;
+export const titles = [
+    'Здам квартиру',
+    'Здам будинок',
+    'Здам сарай',
+    'Здам лофт',
+    'Здам Студію',
+    'Здам гараж',
+];
+
+export const prices = {
+    min:1000,
+    max:5000,
+};
+
+export const rooms = {
+    min:1,
+    max:5,
+};
+
+export const guests = {
+    min:1,
+    max:10,
+};
+
+export const checkTimes = [
+    '12:00',
+    '13:00',
+    '14:00',
+];
+
+export const features = [
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner',
+];
+
+export const types = [
+    'palace',
+    'flat',
+    'house',
+    'bungalow'
+];
+
+export const descriptions = [
+    'Відстань від готелю до пляжу становить 1,9 км, а до залізничного вокзалу- 6,6 км. Відстань до міжнародного аеропорту становить 6 км.',
+    'Центр культури і мистецтва знаходиться в 1,9 км від апартаментів. Відстань до міжнародного аеропорту 16 км.',
+    'Парам особливо подобається розташування — вони оцінили проживання в цьому районі для поїздки вдвоє на 8,5.',
+    'Готель розташований сімейними номерами.',
+    'Найпопулярнішими точками інтересу поряд з ним, включають Сеіганджі храм, Шосен-джи і Шибуя Kuritsu Honcho Park. ',
+    'Рядом з готелем знаходяться парк, музей народного творчості.',
+];
+
+export const photos = [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+export const startLocation = {
+    x:[35.65000, 35.70000],
+    y:[139.70000, 139.80000],
+};
+
+export const digits = 5;
